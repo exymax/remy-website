c94d237965261d17e1c975a8e78279eb12094efa
AlexeyShalikWork <lesha10000ss@gmail.com>
2018-11-09 11:24:24 +0300

    feat: added about us

--- src/components/AboutSection/index.js
+++ src/components/AboutSection/index.js
@@ -3,7 +3,19 @@ import React from 'react';
 import './styles.scss';
 
 export default () => (
-	<div>
-		AboutSection
+	<div className="about-us">
+		<div className="title">
+      what we believe
+		</div>
+
+		<div className="description">
+			<div>
+      	Advances in robotics and machine learning will transform the food industry, just as they are changing many industries now. In the not-too-distant future, the meals we eat will be prepared - chopped, cooked, and served - with the help of  robots. This will save people - and businesses - time, lower costs, improve and standardise quality, and reduce food waste.
+			</div>
+
+			<div>
+				We are using robotic technology to create a ‘chef robot’ that can help in cooking any cuisine or any dish in the world. This could be used for businesses (dark-kitchens, restaurants, factories), or at home, where people will be able to eat restaurant quality meals at a fraction of the cost.
+			</div>
+		</div>
 	</div>
 );
