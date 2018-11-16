0076f6a00e43c97a294ed3da71024492439c67f9
AlexeyShalikWork <lesha10000ss@gmail.com>
2018-11-16 12:27:56 +0300

    feat: animation

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import smoothscroll from 'smoothscroll-polyfill';
 
 import logo from '~image/Remy_logo_white.svg';
 import './styles.scss';
+
+smoothscroll.polyfill();
 
 class Header extends React.PureComponent {
   static propTypes = {
     height: PropTypes.number.isRequired,
     width: PropTypes.number.isRequired,
+  };
+
+  handleScroll = () => {
+    const { height } = this.props;
+
+    window.scroll({ top: height, left: 0, behavior: 'smooth' });
   };
 
   render() {
@@ -31,7 +40,7 @@ class Header extends React.PureComponent {
           robotics technology that takes the routine and inefficiency out of cooking
         </div>
 
-        <div className="discover-more">
+        <div className="discover-more" onClick={this.handleScroll}>
           <div className="ball-section">
             <div className="ball" />
           </div>
